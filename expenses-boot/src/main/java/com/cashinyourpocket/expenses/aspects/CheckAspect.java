7008f9fceef04c3bdd443b2c9cf7a1308e5db2a3
Daniel Núñez Cano <danielncan@ext.inditex.com>
2022-03-28 18:13:21 +0200

    Aspect log

--- expenses-boot/src/main/java/com/cashinyourpocket/expenses/aspects/CheckAspect.java
+++ expenses-boot/src/main/java/com/cashinyourpocket/expenses/aspects/CheckAspect.java
@@ -25,12 +25,12 @@ public class CheckAspect {
       + "execution(* com.cashinyourpocket.expenses.application.service.*.*(..))")
   public void beforeController(JoinPoint joinPoint) {
     List<String> splitName = Arrays.asList(joinPoint.getTarget().getClass().getName().split("\\."));
-    LOGGER.debug("Before to {}.{}",splitName.get(splitName.size()-1),joinPoint.getSignature().getName());
+    LOGGER.info("Before to {}.{}",splitName.get(splitName.size()-1),joinPoint.getSignature().getName());
     Arrays.stream(joinPoint.getArgs()).forEach(arg -> {
       try{
-        LOGGER.debug("Arg: {}",new Gson().toJson(arg));
+        LOGGER.info("Arg: {}",new Gson().toJson(arg));
       } catch (Exception ex) {
-        LOGGER.debug("Arg: ");
+        LOGGER.info("Arg: ");
       }
     });
   }
@@ -38,26 +38,26 @@ public class CheckAspect {
   @Before("@annotation(com.cashinyourpocket.expenses.apirest.auth.CheckLogin)")
   public void beforeLogin(JoinPoint joinPoint) {
     JwtRequest authenticationRequest = (JwtRequest) joinPoint.getArgs()[0];
-    LOGGER.debug("Login User: {}",authenticationRequest.getUsername());
+    LOGGER.info("Login User: {}",authenticationRequest.getUsername());
   }
 
   @After("execution(* com.cashinyourpocket.expenses.apirest.controller.*.*(..)) || "
       + "execution(* com.cashinyourpocket.expenses.application.service.*.*(..))")
   public void afterController(JoinPoint joinPoint) {
     List<String> splitName = Arrays.asList(joinPoint.getTarget().getClass().getName().split("\\."));
-    LOGGER.debug("Return to {}.{}",splitName.get(splitName.size()-1),joinPoint.getSignature().getName());
+    LOGGER.info("Return to {}.{}",splitName.get(splitName.size()-1),joinPoint.getSignature().getName());
   }
 
-//  @AfterReturning(pointcut="execution(* com.cashinyourpocket.expenses.apirest.controller.*.*(..)) || "
-//      + "execution(* com.cashinyourpocket.expenses.application.service.*.*(..))", returning="retVal")
-//  public void logAfterReturningService(Object retVal) {
-//    if(retVal instanceof ResponseEntity){
-//      ResponseEntity responseEntity = (ResponseEntity)retVal;
-//      LOGGER.debug(new Gson().toJson(responseEntity.getBody()));
-//    } else {
-//      LOGGER.debug(new Gson().toJson(retVal));
-//    }
-//
-//  }
+  @AfterReturning(pointcut="execution(* com.cashinyourpocket.expenses.apirest.controller.*.*(..)) || "
+      + "execution(* com.cashinyourpocket.expenses.application.service.*.*(..))", returning="retVal")
+  public void logAfterReturningService(Object retVal) {
+    if(retVal instanceof ResponseEntity){
+      ResponseEntity responseEntity = (ResponseEntity)retVal;
+      LOGGER.info(new Gson().toJson(responseEntity.getBody()));
+    } else {
+      LOGGER.info(new Gson().toJson(retVal));
+    }
+
+  }
 
 }
