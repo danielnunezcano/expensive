b23a6c32d5a27930763778c8e59c0b713fdb3716
Daniel Núñez Cano <danielncan@ext.inditex.com>
2022-03-28 18:37:58 +0200

    Aspect log

--- expenses-api-rest/src/main/java/com/cashinyourpocket/expenses/apirest/dto/UserSecurityDto.java
+++ expenses-api-rest/src/main/java/com/cashinyourpocket/expenses/apirest/dto/UserSecurityDto.java
@@ -5,12 +5,14 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class UserSecurityDto {
   private String user;
   private String rol;
